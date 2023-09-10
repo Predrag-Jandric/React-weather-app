6a60c7229df453768a724d5e05ca6f841c1899ec
predrag jandric <predrag.jandric.bg@gmail.com>
2023-09-10 17:38:23 +0200

    empty project initiated

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function App() {
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=belgrade&units=imperial&appid=7c2d2acdc8a5c07f1b4556c6d79c46c7`
+
+  return (
+    <div className="App">
+    dasdasdasdasd
+    </div>
+  );
+}
+
+export default App;
